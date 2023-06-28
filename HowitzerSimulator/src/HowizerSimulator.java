f29670c89091da413ed333a6f14cd40b65d61a57
David (DongYun)  Kim <pander0616@hotmail.com>
2023-06-28 15:32:58 -0600

    dragForce class implemented (Rough)

---
+++ HowitzerSimulator/src/HowizerSimulator.java
@@ -0,0 +1,5 @@
+package HowitzerSimulator;
+
+public class HowizerSimulator {
+
+}
---
+++ HowitzerSimulator/src/dragForce.java
@@ -0,0 +1,50 @@
+package HowitzerSimulator;
+
+public class dragForce {
+
+	    private double dragCoefficient;
+	    private int fluidDensity;
+	    private int area;
+	    private int flowVelocity;
+
+	    public double calculateDragForce() {
+	    	// Fluid dynamics, the drag force is f_d(t) = -0.5C𝜌Au(t)^2
+	    	double dragForce = -0.5 * dragCoefficient * fluidDensity * area * Math.pow(flowVelocity, 2);
+	        
+	    	return dragForce;
+	    }
+	    
+	    public double getDragCoefficient() {
+	        return dragCoefficient;
+	    }
+	    
+	    public void setDragCoefficient(double dragCoefficient) {
+	        this.dragCoefficient = dragCoefficient;
+	    }
+	    
+	    public double getFluidDensity() {
+	        return fluidDensity;
+	    }
+	    
+	    public void setFluidDensity(int fluidDensity) {
+	        this.fluidDensity = fluidDensity;
+	    }
+	    
+	    public int getArea() {
+	        return area;
+	    }
+	    
+	    public void setArea(int area) {
+	        this.area = area;
+	    }
+	    
+	    public int getFlowVelocity() {
+	        return flowVelocity;
+	    }
+	    
+	    public void setFlowVelocity(int flowVelocity) {
+	        this.flowVelocity = flowVelocity;
+	    }
+
+}
+

9359d2b2a029b2584f002724b215061655ff7d17
Sebastian Lopez Melendrez <90015639+lopezmseb@users.noreply.github.com>
2023-06-29 13:34:32 -0600

    Angle anddragForce getter and setter

--- HowitzerSimulator/src/HowitzerSimulator/HowitzerSimulator.java
+++ HowitzerSimulator/src/HowitzerSimulator/HowitzerSimulator.java
@@ -1,13 +1,26 @@
 package HowitzerSimulator;
 
 public class HowitzerSimulator {
+	private double angle;
 	private DragForce dragForce;
 	
-	public HowitzerSimulator(DragForce dragForce) {
+	public HowitzerSimulator(double angle, DragForce dragForce) {
 		this.dragForce = dragForce;
 	}
 	
 	public DragForce getDragForce() {
 		return dragForce;
+	}
+	
+	public void setDragForce(DragForce dragForce) {
+		this.dragForce = dragForce;
+	}
+	
+	public double getAngle() {
+		return angle;
+	}
+	
+	public void setAngle(double angle) {
+		this.angle = angle;
 	}
 }

e3ab22921f4643d65e8ad766025cf13176a83cf6
Nathan Cameron <62361875+cameronnathan707@users.noreply.github.com>
2023-07-01 14:43:48 -0600

    Added user Input

--- HowitzerSimulator/src/HowitzerSimulator/HowitzerSimulator.java
+++ HowitzerSimulator/src/HowitzerSimulator/HowitzerSimulator.java
@@ -1,4 +1,5 @@
 package HowitzerSimulator;
+import java.util.Scanner;
 
 public class HowitzerSimulator {
 	private double angle;
@@ -9,6 +10,7 @@ public class HowitzerSimulator {
 	private GravitionalForce gravForce;
 	public static int INITIAL_VELOCITY = 10;
 
+
 	public HowitzerSimulator(double angle, int height) {
 		this.angle = angle;
 		this.height = height;
@@ -17,6 +19,7 @@ public class HowitzerSimulator {
 		this.gravForce = new GravitionalForce(MASS);
 //		this.externalForce = new ExternalForce()
 	}
+	
 
 	public double calcLandingTime() {
 		double sinVelocity = INITIAL_VELOCITY * Math.sin(Math.toRadians(angle));
@@ -24,9 +27,11 @@ public class HowitzerSimulator {
 				/ GravitionalForce.GRAVITY;
 	}
 
+
 	public double calcVelocity() {
 		return ((gravForce.calculate()) * calcLandingTime()) / MASS;
 	}
+
 
 	public double calcPosition() {
 		double Vx = INITIAL_VELOCITY * Math.cos(Math.toRadians(angle));
@@ -35,6 +40,15 @@ public class HowitzerSimulator {
 		return (Vx * (Vy + Math.sqrt(Math.pow(Vy, 2) + 2 * GravitionalForce.GRAVITY * height)))
 				/ GravitionalForce.GRAVITY;
 	}
+	
+
+	public double calcMaxHeight() {
+		double Vx = INITIAL_VELOCITY * Math.cos(Math.toRadians(angle));
+		double Vy = INITIAL_VELOCITY * Math.sin(Math.toRadians(angle));
+		
+		return height + ((Math.pow(INITIAL_VELOCITY, 2) * Math.pow( Math.sin(Math.toRadians(angle)), 2))/(2 * GravitionalForce.GRAVITY));
+	}
+
 
 	public DragForce getDragForce() {
 		return dragForce;
@@ -61,12 +75,23 @@ public class HowitzerSimulator {
 	}
 
 	public static void main(String[] args) {
-		double inputAngle = 45;
-		int height = 5;
-
+		double inputAngle;
+		int height;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Input starting height (m): ");
+		height = in.nextInt();
+		
+		System.out.println("Input starting angle (0-90 degrees):");
+		inputAngle = in.nextDouble();
+		
 		HowitzerSimulator howSim = new HowitzerSimulator(inputAngle, height);
 		double position = howSim.calcPosition();
-		System.out.println("Position is " + position);
+		System.out.println("Position is " + position + "m");
+		double airTime = howSim.calcLandingTime();
+		System.out.println("Air Time is " + airTime + "s" );
+		double maxHeight = howSim.calcMaxHeight();
+		System.out.println("Max height is " + maxHeight + "m" );
+		
 
 	}
 }

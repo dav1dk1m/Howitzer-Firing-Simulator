c284a35b6797648a73ae393efa6ac114e721beff
David (DongYun)  Kim <pander0616@hotmail.com>
2023-07-20 13:32:08 -0600

    Added Decision Table Testing

--- HowitzerSimulator/test/HowitzerSimulator/TestHowitzerSimulator.java
+++ HowitzerSimulator/test/HowitzerSimulator/TestHowitzerSimulator.java
@@ -1,14 +1,40 @@
 package HowitzerSimulator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.*;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 
-class TestHowitzerSimulator {
+public class TestHowitzerSimulator {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testCalcPosition1() {
+        HowitzerSimulator howSim = new HowitzerSimulator(45, 0, 3, 10, 10);
+        double[] result = howSim.calcPosition(1);
+        double[] expectedResults = {7.06, 0.0, 0.0};  
+        assertArrayEquals(expectedResults, result, 0.01);
+    }
 
+    @Test
+    public void testCalcPosition2() {
+        HowitzerSimulator howSim = new HowitzerSimulator(45, 90, 3, 10, 10);
+        double[] result = howSim.calcPosition(1);
+        double[] expectedResults = {0.0, 7.06, 0.0};  
+        assertArrayEquals(expectedResults, result, 0.01);
+    }
+
+    @Test
+    public void testCalcPosition3() {
+        HowitzerSimulator howSim = new HowitzerSimulator(45, 0, 3, 10, 10);
+        double[] result = howSim.calcPosition(2);
+        double[] expectedResults = {14.11, 0.0, 0.0};  
+        assertArrayEquals(expectedResults, result, 0.01);
+    }
+
+    @Test
+    public void testCalcPosition4() {
+        HowitzerSimulator howSim = new HowitzerSimulator(45, 90, 3, 10, 10);
+        double[] result = howSim.calcPosition(2);
+        double[] expectedResults = {0.0, 14.11, 0.0};  
+        assertArrayEquals(expectedResults, result, 0.01);
+    }
 }

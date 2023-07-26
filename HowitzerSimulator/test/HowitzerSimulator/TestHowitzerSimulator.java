1a49f0abef7e2b0c863f8e799e424a16d333292a
Nathan Cameron <62361875+cameronnathan707@users.noreply.github.com>
2023-07-25 18:26:14 -0600

    added some testing

--- HowitzerSimulator/test/HowitzerSimulator/TestHowitzerSimulator.java
+++ HowitzerSimulator/test/HowitzerSimulator/TestHowitzerSimulator.java
@@ -22,8 +22,37 @@ class TestHowitzerSimulator {
 		//max test
 		System.out.println("Total Accel: "+howSim.calcHorizontalAcceleration(howSim.airTime));
 		assert(howSim.calcHorizontalAcceleration(howSim.airTime) == 0.1220799060460304);
-		
-		 
+
+	}
+	
+	// Integration Testing 
+	@Test
+	void intTestRange() {
+		HowitzerSimulator howSim = new HowitzerSimulator(45, 0, 3, 85, 10, 15);
+		System.out.println("\n"+ howSim.calcRange(.5));
+		assert(howSim.calcRange(.5) == 3.566182305514064);	
+	}
+	
+	@Test
+	void intTestPosition() {
+		HowitzerSimulator howSim = new HowitzerSimulator(45, 10, 3, 85, 10, 15);
+		System.out.println("\nx"+  howSim.calcPosition(.5)[0]);
+		System.out.println("y"+  howSim.calcPosition(.5)[1]);
+		System.out.println("z"+  howSim.calcPosition(.5)[2]);
+		assert(howSim.calcPosition(.5)[0] == 3.51);
+		assert(howSim.calcPosition(.5)[1] == 0.62);
+		assert(howSim.calcPosition(.5)[2] == 0.0);
+	}
+	
+	@Test
+	void intTestPosition2() {
+		HowitzerSimulator howSim = new HowitzerSimulator(45, 0, 3, 85, 10, 15);
+		System.out.println("\nx"+  howSim.calcPosition(.5)[0]);
+		System.out.println("y"+  howSim.calcPosition(.5)[1]);
+		System.out.println("z"+  howSim.calcPosition(.5)[2]);
+		assert(howSim.calcPosition(.5)[0] == 3.566182305514064);
+		assert(howSim.calcPosition(.5)[1] == 0.0);
+		assert(howSim.calcPosition(.5)[2] == 0.0);
 	}
 
 }
